5243fb1c4d8ecf0a4f008b8b15f34286ff057ac6
Adrian Mendez <mendezadrian149@gmail.com>
2020-04-26 09:51:22 -0400

    adding config options to graphql server

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import {GraphQLServer} from 'graphql-yoga';
-
 import {types as typeDefs, resolvers} from './graphql'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const options = {
+  port: process.env.GRAPHQL_SERVER_PORT,
+  endpoint: process.env.GRAPHQL_END_POINT,
+  playground: process.env.GRAPHQL_PLAYGROUND, 
+}
 
 
 const server = new GraphQLServer({
@@ -8,6 +16,8 @@ const server = new GraphQLServer({
     resolvers
 });
 
-server.start(() => {
- console.log('Server Runing on http://localhost:4000')
-})
+server.start(options, ({ port }) =>
+  console.log(
+    `Server started, listening on port ${port} for incoming requests.`,
+  ),
+)
