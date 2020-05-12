111e2b4bb45574c64c89a1d7fa524c824a62105a
Adrian Mendez <mendezadrian149@gmail.com>
2020-05-11 21:17:34 -0400

    change ways to export mutation and Query of User

--- src/graphql/User/Resolvers/Mutation.ts
+++ src/graphql/User/Resolvers/Mutation.ts
@@ -2,93 +2,87 @@ import { Context } from '../../..'
 import bcrypt from 'bcrypt'
 import { User, Login, genereteToken, Profile, createUserAndRole } from '../User'
 
-
-/**
- * this function create user
- * @param args 
- * @param ctx 
- * @returns Promise<User>
- */
-async function signup(_: void, args: { data: User }, ctx: Context): Promise<User> {
-  const { data }: { data: User } = args
-  return await createUserAndRole(data, ctx)
-}
-
-/**
- * this function update user
- * @param args 
- * @param ctx 
- */
-function updateUser(_: void, args: { data: User; id: number }, ctx: Context): object {
-  const { data, id }: { data: User; id: number } = args
-  const { prisma } = ctx
-
-  return prisma.users.update({ where: { id: Number(id) }, data })
-
-}
-
-/**
- * function login
- * @param args 
- * @param ctx 
- * @returns Promise<Login>
- */
-async function login(_: void, args: Login, ctx: Context): Promise<Login> {
-
-  const { data } = args
-  const { prisma } = ctx
-
-  const user: User | null = await prisma.users.findOne({ where: { email: data.email } })
-
-  if (!user) {
-    throw new Error('incorrect crendentials')
-  }
-
-  const validPassword = await bcrypt.compare(data.password, user.password)
-
-  const role: any = await prisma.user_role
-    .findMany({ where: { user_id: Number(user.id) } })
-
-
-  if (!validPassword) {
-    throw new Error('incorrect crendentials')
-  }
-
-
-  return {
-    token: genereteToken(user, role[0].role_id),
-    data: user
-  }
-
-}
-
-async function uploadProfile(_: void, args: Profile, ctx: Context): Promise<object> {
-  const { data } = args
-  const { prisma } = ctx
-
-
-  const createManyProfile = data.map((profile: Profile) =>
-    prisma.profile.create({
-      data: {
-        institution: profile.institution,
-        position: profile.position,
-        startDate: new Date(profile.startDate),
-        users: {
-          connect: { id: Number(profile.userId) }
-        }
-      }
-    }),
-
-  );
-
-  const profile: Array<object> = await Promise.all(createManyProfile)
-  return profile
-
-}
-
 export const Mutation = {
-  signup,
-  updateUser,
-  login,
-  uploadProfile
+  /**
+  * this function create user
+  * @param args 
+  * @param ctx 
+  * @returns Promise<User>
+  */
+  async signup(_: void, args: { data: User }, ctx: Context): Promise<User> {
+    const { data }: { data: User } = args
+    return await createUserAndRole(data, ctx)
+  },
+
+  /**
+   * this function update user
+   * @param args 
+   * @param ctx 
+   */
+  updateUser(_: void, args: { data: User; id: number }, ctx: Context): object {
+    const { data, id }: { data: User; id: number } = args
+    const { prisma } = ctx
+
+    return prisma.users.update({ where: { id: Number(id) }, data })
+
+  },
+
+  /**
+   * function login
+   * @param args 
+   * @param ctx 
+   * @returns Promise<Login>
+   */
+  async login(_: void, args: Login, ctx: Context): Promise<Login> {
+
+    const { data } = args
+    const { prisma } = ctx
+
+    const user: User | null = await prisma.users.findOne({ where: { email: data.email } })
+
+    if (!user) {
+      throw new Error('incorrect crendentials')
+    }
+
+    const validPassword = await bcrypt.compare(data.password, user.password)
+
+    const role: any = await prisma.user_role
+      .findMany({ where: { user_id: Number(user.id) } })
+
+
+    if (!validPassword) {
+      throw new Error('incorrect crendentials')
+    }
+
+
+    return {
+      token: genereteToken(user, role[0].role_id),
+      data: user
+    }
+
+  },
+
+  async uploadProfile(_: void, args: Profile, ctx: Context): Promise<object> {
+    const { data } = args
+    const { prisma } = ctx
+
+
+    const createManyProfile = data.map((profile: Profile) =>
+      prisma.profile.create({
+        data: {
+          institution: profile.institution,
+          position: profile.position,
+          startDate: new Date(profile.startDate),
+          users: {
+            connect: { id: Number(profile.userId) }
+          }
+        }
+      }),
+
+    );
+
+    const profile: Array<object> = await Promise.all(createManyProfile)
+    return profile
+
+  }
 }
--- src/graphql/User/Resolvers/Query.ts
+++ src/graphql/User/Resolvers/Query.ts
@@ -1,58 +1,55 @@
 import { Context } from '../../..'
 import { User as UserInterface, Profile } from '../User'
 
-/**
- * this function return users
- * if pass id return specific user
- * @param _ 
- * @param args 
- * @param ctx 
- */
-function getUser(_: void, args: UserInterface, ctx: Context): object {
-  const { orderBy, id }: UserInterface = args
+export const Query = {
+  /**
+  * this function return users
+  * if pass id return specific user
+  * @param _ 
+  * @param args 
+  * @param ctx 
+  */
+  getUser(_: void, args: UserInterface, ctx: Context): object {
+    const { orderBy, id }: UserInterface = args
 
-  const { prisma }: Context = ctx
+    const { prisma }: Context = ctx
 
-  if (!id) {
-    return prisma.users.findMany({ orderBy })
-  }
+    if (!id) {
+      return prisma.users.findMany({ orderBy })
+    }
 
-  return prisma.users.findMany({ where: { id: Number(id) } })
+    return prisma.users.findMany({ where: { id: Number(id) } })
+
+  },
+
+  /**
+   * this function return user by email
+   * @param _ 
+   * @param args 
+   * @param ctx 
+   */
+  getUserByEmail(_: void, args: UserInterface, ctx: Context): object {
+    const { email }: UserInterface = args
+    const { prisma }: Context = ctx
+
+    return prisma.users.findOne({ where: { email } })
+  },
 
 }
 
-/**
- * this function return user by email
- * @param _ 
- * @param args 
- * @param ctx 
- */
-function getUserByEmail(_: void, args: UserInterface, ctx: Context): object {
-  const { email }: UserInterface = args
-  const { prisma }: Context = ctx
-
-  return prisma.users.findOne({ where: { email } })
-}
-
-/**
+export const User = {
+  /**
  * return user profile
  * @param _
  * @param args 
  * @param ctx 
  */
-function userProfile(parent: { id: number }, args: Profile, ctx: Context): object {
-  const { id } = parent
-  const { prisma } = ctx
+  userProfile(parent: { id: number }, args: Profile, ctx: Context): object {
+    const { id } = parent
+    const { prisma } = ctx
 
-  return prisma.users.findOne({ where: { id: Number(id) } })
-    .profile()
-}
+    return prisma.users.findOne({ where: { id: Number(id) } })
+      .profile()
+  }
 
-export const Query = {
-  getUser,
-  getUserByEmail
-}
-
-export const User = {
-  userProfile
 }
