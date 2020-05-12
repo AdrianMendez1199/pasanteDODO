f528d49e194df66f62114c002ca43698640a63eb
Adrian Mendez <mendezadrian149@gmail.com>
2020-05-11 21:12:49 -0400

    change ways to export mutation and Query of  job

--- src/graphql/Jobs/Resolvers/Mutation.ts
+++ src/graphql/Jobs/Resolvers/Mutation.ts
@@ -1,51 +1,50 @@
 import { Context } from '../../..'
 import { Job, ApplyJob } from '../Job'
 
-/**
+export const Mutation = {
+  /**
   * this function create Job
   * @param parent 
   * @param args 
   * @param ctx 
   */
-function publishJob(_: void, args: { data: Job }, ctx: Context): object {
-  const { prisma } = ctx
-  const { publishedBy, categoryId, ...rest } = args.data
+  publishJob(_: void, args: { data: Job }, ctx: Context): object {
+    const { prisma } = ctx
+    const { publishedBy, categoryId, ...rest } = args.data
 
-  return prisma.job.create({
-    data: {
-      ...rest,
-      company: { connect: { id: Number(publishedBy) } },
-      categories: { connect: { id: Number(categoryId) } }
+    return prisma.job.create({
+      data: {
+        ...rest,
+        company: { connect: { id: Number(publishedBy) } },
+        categories: { connect: { id: Number(categoryId) } }
+      }
+    })
+  },
+
+
+  async  applyToJob(parent: { id: number }, args: { userId: number; jobId: number }, ctx: Context): Promise<unknown> {
+    const { prisma } = ctx
+    const { userId, jobId } = args
+
+
+    const userApplyToJob: Array<ApplyJob> = await prisma.apply_job.findMany({
+      where: {
+        userId: Number(userId),
+        jobId: Number(jobId)
+      }
+    })
+
+    if (userApplyToJob.length > 0) {
+      throw new Error('you previously applied to this proposal')
     }
-  })
-}
 
+    return prisma.apply_job.create({
+      data: {
+        job: { connect: { id: Number(jobId) } },
+        users: { connect: { id: Number(userId) } }
+      }
+    }).job()
 
-async function applyToJob(parent: { id: number }, args: { userId: number; jobId: number }, ctx: Context): Promise<unknown> {
-  const { prisma } = ctx
-  const { userId, jobId } = args
-
-
-  const userApplyToJob: Array<ApplyJob> = await prisma.apply_job.findMany({
-    where: {
-      userId: Number(userId),
-      jobId: Number(jobId)
-    }
-  })
-
-  if (userApplyToJob.length > 0) {
-    throw new Error('you previously applied to this proposal')
   }
 
-  return prisma.apply_job.create({
-    data: {
-      job: { connect: { id: Number(jobId) } },
-      users: { connect: { id: Number(userId) } }
-    }
-  }).job()
-
-}
-
-export const Mutation = {
-  publishJob, applyToJob
 }
